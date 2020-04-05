d61598241eb6c85ea371aea835cb9e71d1c4f396
banburse <49246001+banburse@users.noreply.github.com>
2020-04-05 16:04:02 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,20 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.get("/GET")
+def root(method):
+    return {"method": 'GET'}
+
+@app.post("/POST")
+def root(method):
+    return {"method": 'POST'}
+
+@app.put("/PUT")
+def root(method):
+    return {"method": 'PUT'}
+
+@app.delete("/DELETE")
+def root(method):
+    return {"method": "DELETE"}
+
