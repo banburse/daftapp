b520be6f65c7a8587542029178847a5184486156
banburse <49246001+banburse@users.noreply.github.com>
2020-04-05 16:37:36 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"message": "Hello World during the coronavirus pandemic!"}
+    return {"message": "Zuzia to lamus"}
+    ##return {"message": "Hello World during the coronavirus pandemic!"}
 
 
 @app.get("/method")
