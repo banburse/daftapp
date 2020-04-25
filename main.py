32ca1644f48a15bd99921301944a53856d290a37
banburse <49246001+banburse@users.noreply.github.com>
2020-04-25 13:57:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,13 +2,13 @@
 # main.py
 from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
-from fastapi.templating import Jinja2Templates
+
 
 
 
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+
 
 class PatientRq(BaseModel):
     name: str
